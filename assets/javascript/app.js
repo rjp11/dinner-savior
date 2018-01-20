3bfaeac39f02c96ff84f2a50299a36e4ca9c9a11
Luis Rosales <guama1239@gmail.com>
2018-01-20 09:39:52 -0600

    google api, search results order changed and button select working, it outputs and object with selected restaurant info called selectResult (#11)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -78,6 +78,7 @@ $(document).ready(function () {
 //THE FOLLOWING JAVASCRIPT IS FOR THE RESTAURANT SEARCH/GOOGLE API
 
 var selectResult = {};
+var picked = {};
 var getPhotoURLByReference = function (ref) {
     return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${ref}&key=AIzaSyCBPyv2jsI299n6NBrTgszQfbPzWI-4ukc`
 };
@@ -99,40 +100,50 @@ $("#search").on("click", function (event) {
         .done(function (response) {
             //console.log(queryURL);
             //console.log(response);
-            var result = response.results;
-            selectResult = response.results;
-            //console.log(results);
-            // Looping through each result item
-            for (var i = 0; i < result.length; i++) {
-                // Creating and storing a div tag
-                var restaurantDiv = $("<div>").text(result[i].name);
-                // Creating a paragraph tag with the result item's rating
-                var p = $("<p>").text("Rating: " + result[i].rating);
-                var address = $("<p>").text("Adress: " + result[i].formatted_address);
-                var selectButton = $("<button id = selected>").text(" Select ");
-                selectButton.addClass("selected");
-                //?????var selectButton = $("<button onclick = selectedFunction(selectResult)>").text(" Select ");???????
-                // Creating and storing an image tag
-                //var restaurantImage = $("<img>");
-                // Setting the src attribute of the image to a property pulled off the result item
-                //restaurantImage.attr("src", results[i].images.fixed_height.url);
-                // Appending the paragraph and image tag to the restaurantDiv
-                //this is where I store the photoreference                 
-                var photoRef = result[i].photos[0].photo_reference;
-                var url = getPhotoURLByReference(photoRef);
-                var img = $("<img>");
-                img.attr("src", url);
-                restaurantDiv.append(p, address, selectButton, img);
-                // Prependng the restaurantDiv 
-                $("#Possible-matches").prepend(restaurantDiv);
+            
+                var result = response.results;
+                console.log(result);
+                // Looping through each result item
+                for (var i = 0; i < result.length; i++) {
+                    // Creating and storing a div tag
+                    picked = result[i].formatted_address;
+                    var restaurantDiv = $("<div>").text(result[i].name);
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + result[i].rating);
+                    var address = $("<p>").text("Adress: " + result[i].formatted_address);
+                    var selectButton = $("<button>").text(" Select ");
+                    selectButton.addClass("selected");
+                    selectButton.attr("data-name", result[i].name);
+                    selectButton.attr("data-rating",result[i].rating);
+                    selectButton.attr("data-address", result[i].formatted_address);
+                    //?????var selectButton = $("<button onclick = selectedFunction(selectResult)>").text(" Select ");???????
+                    // Creating and storing an image tag
+                    //var restaurantImage = $("<img>");
+                    // Setting the src attribute of the image to a property pulled off the result item
+                    //restaurantImage.attr("src", results[i].images.fixed_height.url);
+                    // Appending the paragraph and image tag to the restaurantDiv
+                    //this is where I store the photoreference                 
+                    var photoRef = result[i].photos[0].photo_reference;
+                    var url = getPhotoURLByReference(photoRef);
+                    var img = $("<img>");
+                    img.attr("src", url);
+
+                    restaurantDiv.append(p, address, selectButton, img);
+                    // Prependng the restaurantDiv 
+                    $("#Possible-matches").append(restaurantDiv);
             }
         });
     $("#search-input").val("");
 });
 
 $(document).on("click", ".selected", function () {
-    console.log("the functions works");
-    console.log(selectResult);
+    //console.log("the functions works");
+    selectResult = {
+    name : $(this).attr("data-name"),
+    rating: $(this).attr("data-rating"),
+    address: $(this).attr("data-address")
+    }
+    console.log(selectResult)
 });
 
 //THE FOLLOWING JAVASCRIPT IS FOR THE WAIT-INPUT.HTML PAGE
